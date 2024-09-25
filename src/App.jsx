cd716042d378e406aa9cbdc1340590f022aaf8d3
hosh1dEcho <kaungzanthaw27904@gmail.com>
2024-09-25 12:47:46 +0630

    dark/light mode done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
 import NavBar from "./components/Home/NavBar";
 import Slider from "./components/Home/Slider";
+import { SunIcon,MoonIcon } from "@heroicons/react/16/solid";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   return (
     <div className={`w-full p-4 ${isDarkMode ? "bg-slate-800" : "bg-white"}`}>
-      <button onClick={() => setIsDarkMode(!isDarkMode)} className="mb-4">
-        Dark Mode
-      </button>
+      <div className="flex justify-end mb-4">
+        <button
+          onClick={() => setIsDarkMode(!isDarkMode)}
+          className="p-2 bg-gray-300 rounded-full"
+        >
+          {isDarkMode ? (
+            <SunIcon className="h-6 w-6 text-yellow-500" />
+          ) : (
+            <MoonIcon className="h-6 w-6 text-gray-700" />
+          )}
+        </button>
+      </div>
       <NavBar />
       <Slider />
     </div>
--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -67,7 +67,7 @@ export function NavBar() {
   );
 
   return (
-    <Navbar className="mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4">
+    <Navbar className="mx-auto max-w-screen-xl px-4 py-2 lg:px-8 lg:py-4 rounded-3xl">
       <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
         <Typography
           as="a"
